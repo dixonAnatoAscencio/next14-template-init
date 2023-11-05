1ceb4364162fa017f00c6585c04993e5e4c7a78e
Dixon <dixonjesusanato@gmail.com>
2023-11-05 12:36:00 -0500

    add template init next14

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from "../ui/skeletons";
+
+export default function Loading() {//Si no tengo los datos muestra un loading esto se coloca en la ruta , puede ser el esqueleto del dashboard
+    return <DashboardSkeleton/>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import {Montserrat} from 'next/font/google'
+import {Lusitana} from 'next/font/google'
+
+export const monserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-montserrat',
+})
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--font-lusitana',
+})
